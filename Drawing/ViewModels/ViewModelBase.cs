d91f2888fd2e223ce875ae72118c061088e5f6f2
Андрей Гомонец <andrei.gomonec@gmail.com>
2025-01-27 18:42:36 +0500

    Добавьте файлы проекта.

---
+++ Drawing/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Drawing.ViewModels
+{
+    public class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ Drawing/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Drawing.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
